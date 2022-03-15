012f71ccd766f27b1b8629d0d9b737ba60f6e5c9
Mohit <mohitsajwan0@gmail.com>
2022-03-15 09:04:16 +0530

    handson states

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,37 @@
 //generate component react
 //rfc
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("UpperCase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("onChange() function : Text = " + event.target.value);
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter Text Here");
+  //Wrong way to change state : text = "some value"
+  //right way to change state : setText("some value");
   return (
     <div>
       <div className="mb-3">
-        <h1> {props.heading}</h1>
-        <textarea id="myBox" rows="8" className="form-control"></textarea>
+        <h1>{props.heading}</h1>
+        <textarea
+          id="myBox"
+          rows="8"
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to UpperCase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to UpperCase
+      </button>
     </div>
   );
 }
